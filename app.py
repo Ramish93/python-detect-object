7ccb738f42b2f6a270e6c92a9e46b79a9fd0cc11
Ramish Hassan <ramishshah93@gmail.com>
2022-08-23 11:29:53 +0200

    detecting when object enters the frame

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ video_captured=cv2.VideoCapture(0)
 
 while True:
     check, frame = video_captured.read()
+    status=0
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     gray=cv2.GaussianBlur(gray,(21,21), 0)
@@ -19,14 +20,26 @@ while True:
     thesh_frame= cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thesh_frame = cv2.dilate(thesh_frame, None, iterations=2)
 
+    (cnts,_) = cv2.findContours(thesh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        status=1
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w, y+h), (0, 255,0), 3)
+
     
     cv2.imshow('capture', gray)
     cv2.imshow('delta_frame', delta_frame)
     cv2.imshow('thesh_delta', thesh_frame)
+    cv2.imshow('color frame', frame)
+
 
     key=cv2.waitKey(1)
     if key==ord('q'):
         break
+    print(status)
 
 video_captured.release()
 cv2.destroyAllWindows()
